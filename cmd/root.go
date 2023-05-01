14ab67b9e01f8c111b29ddc23b4a4cc25c0c63cf
Fco Javier Felix <ffelix@inode64.com>
2023-05-01 19:06:20 +0200

    Split cmd into updatemedia

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-	"log"
-	"math"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"time"
-
 	syncmediatrack "github.com/inode64/SyncMediaTrack/lib"
-	"github.com/karrick/godirwalk"
 	"github.com/spf13/cobra"
 )
 
@@ -18,7 +9,6 @@ var (
 	dryRun     bool
 	force      bool
 	geoservice bool
-	mediaDir   string
 	track      string
 )
 
@@ -28,11 +18,6 @@ var rootCmd = &cobra.Command{
 	Long:    `Using a gpx track, analyze a directory with images or movies and add the GPS positions`,
 	Args:    cobra.MinimumNArgs(1),
 	Version: "1.1",
-	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) == 1 {
-			mediaDir = args[0]
-		}
-	},
 }
 
 func init() {
@@ -44,160 +29,5 @@ func init() {
 }
 
 func Execute() {
-	var gpsOld syncmediatrack.Trkpt
-
 	cobra.CheckErr(rootCmd.Execute())
-
-	// check if ffmpeg is installed
-	if !FfmpegInstalled() {
-		fmt.Println(syncmediatrack.ColorRed("Ffmpeg is not installed, checking the GPS position and time of Gopro videos will not be performed"))
-	}
-
-	fileInfo, err := os.Stat(track)
-	if err != nil {
-		log.Fatal(syncmediatrack.ColorRed("No open GPX path"))
-	}
-
-	if fileInfo.IsDir() {
-		syncmediatrack.ReadGPXDir(track)
-	} else {
-		syncmediatrack.ReadGPX(track)
-	}
-
-	if len(syncmediatrack.DataGPX) == 0 {
-		log.Fatal(syncmediatrack.ColorRed("There is no track processed"))
-	}
-
-	err = godirwalk.Walk(mediaDir, &godirwalk.Options{
-		Callback: func(path string, de *godirwalk.Dirent) error {
-			var location syncmediatrack.Trkpt
-
-			if de.IsDir() {
-				return nil // do not remove directory that was provided top-level directory
-			}
-
-			if !syncmediatrack.FileIsMedia(path) {
-				return nil
-			}
-
-			relPath, err := filepath.Rel(mediaDir, path)
-			if err != nil {
-				return err
-			}
-			fmt.Printf("[%v] - ", relPath)
-
-			atime, etime, gtime, err := syncmediatrack.GetMediaDate(path, &gpsOld)
-			if err != nil {
-				fmt.Println(err)
-				return nil
-			}
-
-			if etime.IsZero() {
-				compareDates2(atime, gtime, "A")
-			} else {
-				fmt.Printf("[A] ")
-				compareDates(atime, etime, 30)
-				compareDates2(etime, gtime, "E")
-			}
-
-			date := bestDate(atime, etime, gtime)
-
-			fmt.Printf("| ")
-
-			if gpsOld.Lat == 0 && gpsOld.Lon == 0 {
-				fmt.Printf("No location")
-			} else {
-				fmt.Printf("Lat %v Lon %v Ele %v", gpsOld.Lat, gpsOld.Lon, gpsOld.Ele)
-			}
-			fmt.Printf(" -> ")
-
-			if !syncmediatrack.GetClosesGPS(date, &location) {
-				if gpsOld.Lat != 0 && gpsOld.Lon != 0 {
-					fmt.Println(syncmediatrack.ColorYellow("Update not necessary"))
-				} else {
-					fmt.Println(syncmediatrack.ColorRed("There is no close time to obtain the GPS position"))
-				}
-
-				return nil
-			}
-
-			fmt.Printf("Lat %v Lon %v Ele %v ", location.Lat, location.Lon, location.Ele)
-
-			if geoservice {
-				loc, _ := syncmediatrack.ReverseLocation(location)
-				if len(loc) != 0 {
-					fmt.Printf("(%s)", syncmediatrack.ColorGreen(loc))
-				}
-			}
-			if !force && gpsOld.Lat != 0 && gpsOld.Lon != 0 {
-				fmt.Println(syncmediatrack.ColorYellow("(no update)"))
-				return nil
-			}
-
-			fmt.Println(syncmediatrack.ColorGreen("(updating)"))
-
-			if dryRun {
-				return nil
-			}
-
-			err = syncmediatrack.WriteGPS(location, path)
-			if err != nil {
-				fmt.Println(err)
-				return nil
-			}
-
-			return nil
-		},
-	})
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func bestDate(atime time.Time, etime time.Time, gtime time.Time) time.Time {
-	if !gtime.IsZero() {
-		return gtime
-	}
-	if !etime.IsZero() {
-		return etime
-	}
-
-	return atime
-}
-
-func compareDates(t1 time.Time, t2 time.Time, sec float64) {
-	// remove timezone from time
-	t1, _ = time.Parse("2006-01-02 15:04:05", t1.Format("2006-01-02 15:04:05"))
-	t2, _ = time.Parse("2006-01-02 15:04:05", t2.Format("2006-01-02 15:04:05"))
-
-	diff := math.Abs(t1.Sub(t2).Seconds())
-
-	if diff > sec {
-		fmt.Printf("%s -> ", syncmediatrack.ColorYellow(t1.Format("02/01/2006 15:04:05")))
-	} else {
-		fmt.Printf("%s -> ", t1.Format("02/01/2006 15:04:05"))
-	}
-}
-
-func compareDates2(old time.Time, gtime time.Time, prefix string) {
-	fmt.Printf("[%s] ", prefix)
-	if gtime.IsZero() {
-		fmt.Printf("%s ", old.Format("02/01/2006 15:04:05"))
-	} else {
-		compareDates(old, gtime, 80)
-		fmt.Printf("[G] %s ", gtime.Format("02/01/2006 15:04:05"))
-	}
-}
-
-// Function to check if ffmpeg is installed
-func FfmpegInstalled() bool {
-	path, err := exec.LookPath("ffmpeg")
-	if err != nil {
-		return false
-	}
-
-	cmd := exec.Command(path, "-version")
-	err = cmd.Run()
-
-	return err == nil
 }
